18c24a9c177a718faa41274cd18fbd18d5c450e1
GKiarie <igitaukiarie@gmail.com>
2023-07-19 19:12:39 +0300

    incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,17 @@
 import redis
 import uuid
 from typing import Union, Callable
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """Ftn that returns a callable"""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -13,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key using uuid"""
         key = str(uuid.uuid4())
